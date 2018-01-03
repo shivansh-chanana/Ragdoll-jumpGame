b501118e11e6a918fd7d68815a9d59afe41a6d93
shivansh-chanana <shivansh.chanana@gmail.com>
2018-01-04 01:06:24 +0530

    Update v1.4
    Collision Added
    Restart Object Added
    LevelSize Increased

---
+++ Project_R4/Assets/Scripts/controllerForce.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controllerForce : MonoBehaviour {
+
+	#region Variables Initialize
+	public Vector3 moveForce = new Vector3 (0, 0, 0);
+	public Vector3 jumpForce = new Vector3 (0, 0, 0);
+
+	public Rigidbody rb;
+
+	private bool colWithWalls = false;
+
+	#endregion
+
+	void Start ()	 {
+		rb = GetComponent<Rigidbody> ();
+	}
+		
+	void Update(){
+		if (Input.GetKeyDown (KeyCode.Space)) jump ();
+	}
+	void FixedUpdate () {
+		rb.AddTorque (-moveForce * Time.deltaTime * 60);
+	}
+
+	#region Collision Stuff
+	void OnTriggerEnter(Collider triggerTarget){
+		if (triggerTarget.tag == "Walls") {
+			Debug.Log ("CAN jump Now");
+			colWithWalls = true;
+		}
+
+		if (triggerTarget.tag == "Finish") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+		}
+	}
+
+	void OnTriggerStay(Collider triggerTarget){
+		if (triggerTarget.tag == "Walls") {
+			Debug.Log ("CAN jump Now");
+			colWithWalls = true;
+		}
+	}
+
+	void OnTriggerExit(Collider triggerTarget){
+			Debug.Log ("CANNOT jump Now");
+			colWithWalls = false;
+	}
+	#endregion
+
+	public void jump(){
+		if(colWithWalls) rb.AddForce (jumpForce * Time.deltaTime * 60,ForceMode.Impulse);
+	}
+
+}
